2b233ba44db86613cdc1f03f859ca76c25c07d70
shruti1299 <65611000+shruti1299@users.noreply.github.com>
2021-05-31 15:09:21 +0530

    Update weight.height.py

--- weight.height.py
+++ weight.height.py
@@ -9,14 +9,14 @@ x = x.values
 x = x.reshape(10000,1)
 y = y.values
 y = y.reshape(10000,1)
-from sklearn.linear_model import LinearRegression 
+
 mind = LinearRegression()
 mind.fit(x,y)
 
 while True:
             input1 = float(input("enter height for which you want to predict weight: "))
             print("Your estimated Salary:", mind.predict( [[ input1 ]] ) )
-            quit = input("\nPress q to exit & to continue press Enter:")
+            quit = input("\nPress q to Exit & to continue press Enter:")
 	    if quit == "q":
 	 	   break
             
